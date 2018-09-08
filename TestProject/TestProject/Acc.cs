1928566216d67548c0846bf39cf43fd8b43308de
JackyEnclave <evgeny_pchelyakov@mail.ru>
2018-09-08 22:52:01 +0300

    Calculators Create

---
+++ TestProject/TestProject/Acc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    class Acc
+    {
+        private string _name;
+        private string _password;
+        private int _age;
+        private int _weight;
+        private int _height;
+        public string Name { get => _name; set => _name = value; }
+        public string Password { get => _password; set => _password = value; }
+        public int Age { get => _age; set => _age = value; }
+        public int Weight { get => _weight; set => _weight = value; }
+        public int Height { get => _height; set => _height = value; }
+
+    }
+}
