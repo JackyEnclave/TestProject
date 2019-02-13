693b6fafd4fcd8102a69f903e7f97c5feace67e1
Pchelyakov Evgeny <evgeny_pchelyakov@mail.ru>
2019-02-13 17:43:21 +0300

    user params window

--- TestProject/CaloryCalculator/Model/Acc.cs
+++ TestProject/CaloryCalculator/Model/Acc.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace CaloryCalculator
 {
+    [Serializable]
     class Acc
     {
         private string _name;
-        private string _password;
         private int _age;
         private int _weight;
         private int _height;
         public string Name { get => _name; set => _name = value; }
-        public string Password { set => _password = value; }
         public int Age { get => _age; set => _age = value; }
         public int Weight { get => _weight; set => _weight = value; }
         public int Height { get => _height; set => _height = value; }
 
+        public const string ACCPATH = @"C:\Users\Public\Calorizzation\acc.json";
     }
 }
